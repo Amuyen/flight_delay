179e6e4b8909d58d75a470b5217a8a63ef9cf383
Amuyen <pablo.nebel@gmail.com>
2023-05-23 20:47:29 +0100

    functions

--- Notebooks/myfunctions.py
+++ Notebooks/myfunctions.py
@@ -1,25 +1,60 @@
-import numpy as np, pandas as pd, seaborn as sns, matplotlib as plt
+import numpy as np, pandas as pd, seaborn as sns, matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-import datetime
+from datetime import datetime, timedelta
 from sklearn.model_selection import train_test_split
 from tinydb import TinyDB, Query
 
-def date_cols(df:pd.DataFrame,x:str):
-    df['Month']=df[x].strftime('%m').astype('int')
-    df['DayofMonth']=df[x].strftime('%d').astype('int')
-    df['DayOfWeek']=df[x].strftime('%u').astype('int')
+def cols_types(df:pd.DataFrame):
+    df['Month']=[datetime.strftime(df['FlightDate'][i],'%m') for i in df.index]
+    df['DayOfWeek']=[datetime.strftime(df['FlightDate'][i],'%u') for i in df.index]
+    df['DayOfMonth']=[datetime.strftime(df['FlightDate'][i],'%d') for i in df.index]
+    df['CRSDepTime']=df['CRSDepTime'].apply(lambda x: convert_to_min(x))
+    df['CRSArrTime']=df['CRSArrTime'].apply(lambda x: convert_to_min(x))
+    df['Flight_Number_Marketing_Airline']=df['Flight_Number_Marketing_Airline'].astype('object')
+    df['Flight_Number_Operating_Airline']=df['Flight_Number_Operating_Airline'].astype('object')
+    return df
 
-def create_db(path:str):
-    df=pd.read_parquet('../Data/Raw/Data.parquet')
+def convert_to_min(x):
+    if x<10:
+        string= '00:0'+str(x)
+    elif x<60:
+        string= '00:'+str(x)
+    elif x<960:
+        string= '0'+str(x)[0]+':'+str(x)[1:]
+    else:
+        string=str(x)[0:2]+':'+str(x)[2:]
+    dt=datetime.strptime(string,'%H:%M')+timedelta(days=25569)
+    return dt.timestamp()
+
+
+def create_db(df):
     db=TinyDB('db.json')
     for i in len(df):
         db.insert({c:df[c][i] for c in df.columns})
-        
-def outcome(df_):
-    if df_['Cancelled']:
+
+def outcome(df):
+    if df['Cancelled']:
         return 'Cancelled'
-    if df_['ArrDelayMinutes']>0:
+    if df['ArrDelayMinutes']>0:
         return 'Delayed'
-    if df_['Diverted']:
+    if df['Diverted']:
         return 'Diverted'
     return 'OK'
+
+def filtered(df):
+    thresh=max(df['FlightDate'])
+    df=df[thresh<(df['FlightDate']+pd.Timedelta(days=730))]
+    return df[~df['Diverted']]
+
+def add_marketing(df):
+    companies=df.groupby(['IATA_Code_Operating_Airline','Airline']).agg('count').reset_index()
+    return {companies['IATA_Code_Operating_Airline'][i]:companies['Airline'][i] for i in range(len(companies))}
+
+def clean(df):
+    if df['FlightDate'].dtype=='object':
+        df['FlightDate']=pd.to_datetime(df['FlightDate'])
+    df=filtered(df)
+    df['result']=df.apply(lambda x: outcome(x), axis=1)
+    df=cols_types(df)
+    df=df[['Month','DayofMonth','DayOfWeek','Origin', 'Dest','IATA_Code_Marketing_Airline','IATA_Code_Operating_Airline','Flight_Number_Marketing_Airline','Flight_Number_Operating_Airline','ArrDelayMinutes','Distance','CRSDepTime','CRSArrTime','result']] 
+    return df
